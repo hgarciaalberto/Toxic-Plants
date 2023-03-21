79c214c839e3772bd658acf977758caf0faeecae
Alberto Hidalgo <a.hidalgo@waracle.com>
2023-03-21 15:42:41 +0000

    Initial commit

---
+++ app/src/main/java/com/waracle/vision/toxicplants/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.waracle.vision.toxicplants
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.waracle.vision.toxicplants.ui.theme.ToxicPlantsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToxicPlantsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    ToxicPlant()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ToxicPlant() {
+    Text(text = "Hello!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ToxicPlantsTheme {
+        ToxicPlant()
+    }
+}
